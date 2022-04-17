9d4c725e9759e05b9989e50fc572f8d70c627409
Shashank Acharya <acharya.s@icloud.com>
2022-04-17 17:13:39 +0800

    reset

--- src/services/createOrderList.js
+++
@@ -1,7 +0,0 @@
-const createOrderList = (start, maxi) => {
-    const end = [...Array(maxi - start).keys()].map(x => x + start)
-    const begin = [...Array(start).keys()]
-    end.push(...begin)
-    return end
-}
-export default createOrderList
--- src/services/resetDeclarations.js
+++
@@ -1,9 +0,0 @@
-const resetDeclarations = (players, setPlayers) => {
-    const newPlayers = [...players]
-    newPlayers.forEach(p => {
-        p.declaration = 0
-        p.obtained = 0
-    })
-    setPlayers(newPlayers)
-}
-export default resetDeclarations
